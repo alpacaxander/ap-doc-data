5970e748e4ed64f4e2bf7058dca9c3f4b3880e8f
Alexander <alexander.dean.paulsell@gmail.com>
2019-05-29 21:17:02 -0700

    just initializer

---
+++ src/main/java/alexander/paulsell/documentdata/DocumentdataApplication.java
@@ -0,0 +1,13 @@
+package alexander.paulsell.documentdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentdataApplication.class, args);
+	}
+
+}
