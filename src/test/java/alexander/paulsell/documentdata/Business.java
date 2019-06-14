76bc5a192bc0687c7e5a97a56dd9e42fb6b55b9a
Alexander <alexander.dean.paulsell@gmail.com>
2019-06-13 22:59:51 -0700

    intermittent

--- src/test/java/alexander/paulsell/documentdata/Business.java
+++ src/test/java/alexander/paulsell/documentdata/Business.java
@@ -8,5 +8,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Business {
+    // Create
 
+    // Read
+
+    // Update
+
+    // Delete
 }
